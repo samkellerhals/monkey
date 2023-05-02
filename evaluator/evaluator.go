ad974a0b1bc3904521a29868318a03fc198a6967
samkellerhals <kellerhalssamuel@gmail.com>
2023-05-02 15:09:14 +0200

    Add booleans and null

---
+++ evaluator/evaluator.go
@@ -0,0 +1,45 @@
+package evaluator
+
+import (
+	"monkey/ast"
+	"monkey/object"
+)
+
+var (
+	NULL  = &object.Null{}
+	TRUE  = &object.Boolean{Value: true}
+	FALSE = &object.Boolean{Value: false}
+)
+
+func Eval(node ast.Node) object.Object {
+	switch node := node.(type) {
+
+	case *ast.Program:
+		return evalStatements(node.Statements)
+	case *ast.ExpressionStatement:
+		return Eval(node.Expression)
+	case *ast.IntegerLiteral:
+		return &object.Integer{Value: node.Value}
+	case *ast.Boolean:
+		return nativeBoolToBooleanObject(node.Value)
+	}
+
+	return nil
+}
+
+func nativeBoolToBooleanObject(value bool) object.Object {
+	if value {
+		return TRUE
+	}
+	return FALSE
+}
+
+func evalStatements(statements []ast.Statement) object.Object {
+	var result object.Object
+
+	for _, stmt := range statements {
+		result = Eval(stmt)
+	}
+
+	return result
+}
